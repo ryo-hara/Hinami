0dae9b910fa7d014bcc0a1ae689e77606454a739
ryo <akstkr@gmail.com>
2025-06-12 03:06:38 +0900

    残りのファイルを追加する

---
+++ Source/Hinami/HinamiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HinamiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHinamiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHinamiGameMode();
+};
+
+
+

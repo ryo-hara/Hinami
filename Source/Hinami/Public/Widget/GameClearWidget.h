e1bd335c9ecc41e4fd4de4c64d6eb44366fbe648
ryo <akstkr@gmail.com>
2025-06-24 02:13:58 +0900

    Widget追加

---
+++ Source/Hinami/Public/Widget/GameClearWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameClearWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HINAMI_API UGameClearWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
